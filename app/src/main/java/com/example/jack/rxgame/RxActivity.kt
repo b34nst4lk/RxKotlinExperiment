5ad345714b10cffff70f81151255bd3289ad2063
Jack <jsim89@gmail.com>
2018-10-14 15:56:47 +0800

    cleaned up and simplified code

--- app/src/main/java/com/example/jack/rxgame/RxActivity.kt
+++ app/src/main/java/com/example/jack/rxgame/RxActivity.kt
@@ -1,70 +1,48 @@
 package com.example.jack.rxgame
 
-import android.annotation.SuppressLint
-import android.gesture.Gesture
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
 import io.reactivex.subjects.PublishSubject
-import org.reactivestreams.Subscriber
 import java.util.*
 
 class RxActivity : AppCompatActivity() {
-    lateinit var singleTapView : TextView
-    lateinit var doubleTapView : TextView
-    lateinit var swipeUpView: TextView
-    lateinit var swipeDownView: TextView
-    lateinit var touchArea: TextView
-    lateinit var motionEvents: PublishSubject<MotionEvent>
-    lateinit var subscriber: Subscriber<MotionEvent>
-    var rnd : Random = Random()
+    private var motionEvents: PublishSubject<MotionEvent> = PublishSubject.create()
+    private var rnd : Random = Random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx)
-        initializeViews()
-
-        motionEvents = PublishSubject.create()
-
-        assignEventHandlers()
+        assignTouchListener()
         subscribeToEvent()
     }
 
-    fun initializeViews() {
-        singleTapView = findViewById(R.id.text1)
-        doubleTapView = findViewById(R.id.text2)
-        swipeUpView = findViewById(R.id.text3)
-        swipeDownView= findViewById(R.id.text4)
-        touchArea = findViewById(R.id.text5)
-    }
-
-    fun assignEventHandlers() {
-        touchArea.setOnTouchListener { view, motionEvent ->
-            motionEvents.onNext(motionEvent)
-            true
+    private fun assignTouchListener() {
+        findViewById<TextView>(R.id.touch_area_view).setOnTouchListener() {
+            view: View, event : MotionEvent ->
+                motionEvents.onNext(event)
+                true
         }
     }
 
-    fun subscribeToEvent() {
-        motionEvents.subscribe { motionEvent ->
-            Log.i("Event", motionEvent.toString())
-            singleTapView.setBackgroundColor(randomizeColors())
-            doubleTapView.setBackgroundColor(randomizeColors())
-            swipeUpView.setBackgroundColor(randomizeColors())
-            swipeDownView.setBackgroundColor(randomizeColors())
-        }
+    private fun subscribeToEvent() {
+        motionEvents
+            .filter { it.action != MotionEvent.ACTION_MOVE }
+            .subscribe { event ->
+                Log.i("Pointer Id", "Finger " + event.getPointerId(event.actionIndex).toString())
+                findViewById<TextView>(R.id.single_tap_view).setBackgroundColor(getRandomColor())
+            }
     }
 
-    fun randomizeColors() : Int {
+    private fun getRandomColor() : Int {
         return Color.rgb(
-                rnd.nextInt(255),
-                rnd.nextInt(255),
-                rnd.nextInt(255)
+            rnd.nextInt(255),
+            rnd.nextInt(255),
+            rnd.nextInt(255)
         )
     }
 }
